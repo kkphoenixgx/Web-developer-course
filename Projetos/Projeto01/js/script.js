d8d70ee275ddf5d25772f0c7d51831c5934c3068
kkphoenixgx <you@example.com>
2021-06-01 15:52:28 -0300

    01/06/21-AulasMudadas

---
+++ Projetos/Projeto01/js/script.js
@@ -0,0 +1,20 @@
+//exemplos de comandos
+alert("Olá Hcode")
+
+var resposta = confirm("deseja excluir?")
+
+if (resposta = true){
+
+	console.log ("trolei");
+
+}
+console.log(resposta);
+
+//calculos em js
+
+function somar(a,b ){
+
+	return a+b
+}
+
+console.log(somar(2,5));
