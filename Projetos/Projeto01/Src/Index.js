74d503275db7cc8932fccc1a5be3af02579b2a03
Kauã Alves <kauaalvesworkplace@gmail.com>
2022-11-26 19:46:33 -0300

    remaking

--- Projetos/Projeto01/Src/Index.js
+++
@@ -1,5 +0,0 @@
-import {default as Controller} from "./controller/ControllerIndex.js"
-
-let headerElement = document.querySelector("header")
-let Page = headerElement.dataset.file
-window.site = new Controller(Page);
